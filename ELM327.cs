09d130a10b96391322996ee3df33697fa7244b91
Chris <chris3x33g@gmail.com>
2020-09-12 09:27:52 -0500

    Add base code

---
+++ ELM327.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ELM327CSL
+{
+    public abstract class ELM327
+    {
+    }
+}
---
+++ ELM327S.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ELM327CSL
+{
+    public class ELM327S : ELM327
+    {
+        public ELM327S() 
+        {
+            Console.WriteLine("ELM327S Lib");
+        }
+    }
+}
